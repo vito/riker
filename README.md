69c0162e6dccc2678a90502e417ee44137557ff5
Onsi Fakhouri <onsijoe@gmail.com>
2014-11-24 08:31:38 -0800

    Beam me down O'Brien

---
+++ README.md
@@ -0,0 +1,19 @@
+# Riker - Containerized reconnaisance
+
+Riker is a web app that boldly goes into a container.
+
+By default, Riker listens on 8080, unless you set the `PORT` environment variable.
+
+## Getting Riker
+
+Riker is available as part of:
+
+docker:///onsi/away-team
+
+(see the [away-team](https://github.com/1701-diego/away-team) repo)
+
+Or as a stand-alone tarball at:
+
+http://onsi-public.s3.amazonaws.com/riker.tar.gz
+
+Examples of deploying Riker are in [Laforge](https://github.com/1701-diego/laforge)
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strings"
+	"text/template"
+	"time"
+)
+
+var rikers = []string{
+	"http://i.imgur.com/IObN0tJ.gif",
+	"http://i.imgur.com/DlmpR08.gif",
+	"http://i.imgur.com/E9xIncq.gif",
+	"http://i.imgur.com/hCcuFRm.gif",
+	"http://i.imgur.com/Axc9V1j.gif",
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Riker here...")
+		out := "<dl>"
+		for _, env := range os.Environ() {
+			kv := strings.Split(env, "=")
+			out += "<dt>" + kv[0] + "</dt>"
+			out += "<dd>" + kv[1] + "</dd>"
+		}
+		out += "</dl>"
+		randomizer := rand.New(rand.NewSource(time.Now().UnixNano()))
+		styledTemplate.Execute(w, Content{
+			Envs:  `<div class="envs">` + out + `</div>`,
+			Riker: rikers[randomizer.Intn(len(rikers))],
+		})
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Printf("Riker here, listening on port %s\n", port)
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+var styledTemplate = template.Must(template.New("riker").Parse(`
+<html>
+<head>
+<style>
+body {
+    font-family: "helveticaneue-light";
+    font-size: 16px;
+    color: #333;
+    position: absolute;
+    margin:0;
+    width:100%;
+    height:100%;
+}
+
+dt {
+  color:#777;
+}
+
+img {
+    margin:10px
+}
+
+.envs {
+  margin:10px
+}
+
+</style>
+</head>
+<body>
+<img src="{{.Riker}}">
+{{.Envs}}
+</body>
+</html>
+`))
+
+type Content struct {
+	Envs  string
+	Riker string
+}
